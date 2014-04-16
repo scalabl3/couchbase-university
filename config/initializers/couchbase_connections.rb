9b5eb3d4532b6027e8700313ba8c180d19d30dde
Jasdeep Jaitla <jasdeep@scalabl3.com>
2014-04-16 13:31:52 -0700

    figuring out couchbase and puma

--- config/initializers/couchbase_connections.rb
+++ config/initializers/couchbase_connections.rb
@@ -1,17 +1,17 @@
 require 'couchbase'
 
-Rails.logger.info "couchbase_connections.rb" if defined?(Rails) and Rails.logger
-puts ENV['cbu_couchbase_servers']
-
-CBU = Couchbase.new(bucket: "cbu", node_list: ENV['cbu_couchbase_servers'].split(","))
-CBD = Couchbase.new(bucket: "cbdocs", node_list: ENV['cbu_couchbase_servers'].split(","))
-
-CBU.quiet = true
-CBD.quiet = true 
+# Rails.logger.info "couchbase_connections.rb" if defined?(Rails) and Rails.logger
+# puts ENV['cbu_couchbase_servers']
+# 
+# CBU = Couchbase.new(bucket: "cbu", node_list: ENV['cbu_couchbase_servers'].split(","))
+# CBD = Couchbase.new(bucket: "cbdocs", node_list: ENV['cbu_couchbase_servers'].split(","))
+# 
+# CBU.quiet = true
+# CBD.quiet = true 
 
 
 Rails.logger.info CBU.inspect if defined?(Rails) and Rails.logger
 Rails.logger.info CBD.inspect if defined?(Rails) and Rails.logger
 
-DocsNavTree.generate
-RenderNavTree.generate
+#DocsNavTree.generate
+#RenderNavTree.generate
