772d5f01d89549189d98c9a8065cbaaa11b5226b
Jasdeep Jaitla <jasdeep@scalabl3.com>
2014-04-16 13:32:41 -0700

    figuring out couchbase and puma

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,10 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+require 'dotenv'
+Dotenv.load! ".env"  
+puts ENV['cbu_couchbase_servers']
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
