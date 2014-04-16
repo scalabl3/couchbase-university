14bd692ba3dccf87a617bf2f19757f6d6b74220f
Jasdeep Jaitla <jasdeep@scalabl3.com>
2014-04-16 13:21:16 -0700

    update to use dotenv

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 require 'dotenv'
 Dotenv.load! ".env"
-CB_SERVERS=ENV['cbu_couchbase_servers'].split(",")
-puts ENV['cbu_couchbase_servers']
 
 # Initialize the Rails application.
 CouchbaseUniversity::Application.initialize!
