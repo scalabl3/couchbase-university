3dfa2c5f1343bd45aeec10fac1ace4b7609332bb
Jasdeep Jaitla <jasdeep@scalabl3.com>
2014-04-16 13:27:01 -0700

    figuring out couchbase and puma

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-require 'dotenv'
-Dotenv.load! ".env"
-
 # Initialize the Rails application.
 CouchbaseUniversity::Application.initialize!
--- config/puma.rb
+++ config/puma.rb
@@ -22,9 +22,9 @@ on_worker_boot do
 	Dotenv.load! ".env"  
 	puts ENV['cbu_couchbase_servers']
 
-	CBU = Couchbase.new(bucket: "cbu", node_list: ENV['cbu_couchbase_servers'].split(","))
-	CBD = Couchbase.new(bucket: "cbdocs", node_list: ENV['cbu_couchbase_servers'].split(","))
+	#CBU = Couchbase.new(bucket: "cbu", node_list: ENV['cbu_couchbase_servers'].split(","))
+	#CBD = Couchbase.new(bucket: "cbdocs", node_list: ENV['cbu_couchbase_servers'].split(","))
 
-	CBU.quiet = true
-	CBD.quiet = true 
+	#CBU.quiet = true
+	#CBD.quiet = true 
 end
