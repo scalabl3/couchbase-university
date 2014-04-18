5fc5aeec2fecee313b243d54e9b248be7513f6f5
Jasdeep Jaitla <jasdeep@scalabl3.com>
2014-04-17 20:07:45 -0700

    change puma to single worker for now

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,8 @@
-workers Integer(ENV['PUMA_WORKERS'] || 3)
+workers Integer(ENV['PUMA_WORKERS'] || 1)
 threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 12)
 
-preload_app!
+#not sure about this... might be messing the couchbase connections up
+#preload_app!
 
 rackup                DefaultRackup
 #port                 ENV['port_cbmodels'] || 3001
