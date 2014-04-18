29e292e46ad0428dad799b25e71bc5e8f3c6bcf7
Jasdeep Jaitla <jasdeep@scalabl3.com>
2014-04-18 12:09:37 -0700

    add user controller, views, nav
    navigation, profile page, settings page, notifications page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@
 CONTENT_LINK_PREFIX = '/d'
 
 CouchbaseUniversity::Application.routes.draw do
-  
+
+	get '/u/:user' => 'user#profile'
+  get '/u/profile' => 'user#profile'
+  get '/u/notifications' => 'user#notifications'
+  get '/u/settings' => 'user#settings'
+
   get "/qa" => 'qa#index'
 	get "/qanda" => 'qa#index'  
 	get "/q-and-a" => 'qa#index'  
